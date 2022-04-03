f29b6046500b51d93ec8b586604a0a9895581559
Bilal <39632030+aminajao@users.noreply.github.com>
2022-04-03 13:53:28 +0100

    fix: removed unused items

--- src/containers/SideBar/SideBar.jsx
+++ src/containers/SideBar/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Logo, SideBar } from "../../components";
 import { Container } from "./Styled";
 import { sideLinks } from "../../constants/data";
